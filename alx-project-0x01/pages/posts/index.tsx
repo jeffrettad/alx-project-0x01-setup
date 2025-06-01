a4364bc8644ed9fea40273852417d9df38c74a38
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-06-01 13:57:59 -0700

    Update index.tsx

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,3 +1,4 @@
+import PostCard from", "import Header", "import { PostProps }", "export async function getStaticProps()", "https://jsonplaceholder.typicode.com/posts"]
 const Posts: React.FC = () => {
   return <div className="p-8">Posts Page</div>;
 };
