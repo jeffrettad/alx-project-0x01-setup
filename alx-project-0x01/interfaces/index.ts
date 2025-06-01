cbdc0f01d26abac6caddc0d38bd842e30a4aa1a5
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-06-01 14:31:54 -0700

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,4 +1,35 @@
 
+
+export interface UserData {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface UserModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSave: (user: UserData) => void;
+}
+
 export interface UserProps {
     
   id: 1;
